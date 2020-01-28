4841093c4f0ffb07f8b227ca73dc15c987ca26c8
Girish Gopaul <ggopaul0510@gmail.com>
2020-01-28 07:05:11 +0400

    Correct tab indent

--- src/TodoApp.svelte
+++ src/TodoApp.svelte
@@ -1,26 +1,26 @@
 <script>
-	import TodoList from './TodoList.svelte';
-	import TodoAdder from './TodoAdder.svelte';
-	
-	let todos = [];
+  import TodoList from './TodoList.svelte';
+  import TodoAdder from './TodoAdder.svelte';
 
-	function addTodo({ detail: title }) {
-		todos = [...todos, { title, completed: false }];
-	}
+  let todos = [];
 
-	function toggleTodo({ detail }) {
-		const index = todos.findIndex(({ title }) => title === detail);
-		todos[index].completed = !todos[index].completed;
-		todos = [...todos];
-	}
+  function addTodo({ detail: title }) {
+    todos = [...todos, { title, completed: false }];
+  }
 
-	function deleteTodo({ detail }) {
-		todos = todos.filter(({ title }) => title !== detail);
-	}
+  function toggleTodo({ detail }) {
+    const index = todos.findIndex(({ title }) => title === detail);
+    todos[index].completed = !todos[index].completed;
+    todos = [...todos];
+  }
+
+  function deleteTodo({ detail }) {
+    todos = todos.filter(({ title }) => title !== detail);
+  }
 </script>
 
 <main class="container">
-	<h1>Svelte Todo App</h1>
-	<TodoAdder on:todo={addTodo} />
-	<TodoList items={todos} on:toggle={toggleTodo} on:delete={deleteTodo} />
+  <h1>Svelte Todo App</h1>
+  <TodoAdder on:todo={addTodo} />
+  <TodoList items={todos} on:toggle={toggleTodo} on:delete={deleteTodo} />
 </main>
